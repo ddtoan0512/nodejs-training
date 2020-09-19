6c77f467669fa41dd7a92f0687f3f8b75d3a1ac6
Toan Dang <ddtoan991205@gmail.com>
2020-09-19 10:06:18 +0700

    Init project

---
+++ app.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const port = 6969;
+
+app.post('/', (req, res) => {
+    res.json({ username: 'Toan' })
+});
+
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+})
