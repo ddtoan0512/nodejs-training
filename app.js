d59d6eb65fe6199710792f6f7784f848b998cccc
Toan Dang <ddtoan991205@gmail.com>
2020-09-25 00:34:52 +0700

    change port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = process.env.PORT | 6969;
+const port = 3000;
 
 const bodyParse = require('body-parser');
 
