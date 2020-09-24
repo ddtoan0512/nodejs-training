ccce9592adad55c5c38ff0484d9a54b66cf507d9
Toan Dang <ddtoan991205@gmail.com>
2020-09-24 16:32:50 +0700

    fix middleware auth

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -2,9 +2,13 @@ const jwt = require('jsonwebtoken');
 const db = require('../models');
 
 module.exports.isAuth = async (req, res, next) => {
-    let token = req.header('authorization').replace('Bearer ', '');
-    // console.log("=====Middleware======");
-    // console.log(token);
+    let token = req.headers.authorization;
+
+    if(!token){
+        res.status(401).send({
+            message: 'Token not found!'
+        })
+    };
 
     try{
         const data = jwt.verify(token, process.env.JWT_TOKEN_SECRET);
